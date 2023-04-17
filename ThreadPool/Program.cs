638598f1d8c94b64270103628f9ba4bdfa089036
Tue Munch Madsen <tuepvm@gmail.com>
2023-04-17 12:34:39 +0200

    Done

--- ThreadPool/Program.cs
+++ ThreadPool/Program.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
-namespace ThreadPool
+namespace ThreadPoolOpg
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Stopwatch mywatch = new Stopwatch();
+
+            Console.WriteLine("Thread Pool Execution");
+
+            mywatch.Start();
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+            mywatch.Reset();
+
+            Console.WriteLine("Thread Execution");
+
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+
+            Console.Read();
+        }
+        static void Process(object callback)
+        {
+        }
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
         }
     }
 }
