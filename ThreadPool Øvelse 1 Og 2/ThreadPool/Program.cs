fd88084edc541cd8e247d66862575d6d793142d5
PandaOnCaffeine <110375288+PandaOnCaffeine@users.noreply.github.com>
2023-04-17 12:51:44 +0200

    Add files via upload

---
+++ ThreadPool Øvelse 1 Og 2/ThreadPool/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ThreadPool_Øvelse_1_Og_2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stopwatch mywatch = new Stopwatch();
+
+            Console.WriteLine("Thread Pool Execution");
+
+            mywatch.Start();
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+            mywatch.Reset();
+
+            Console.WriteLine("Thread Execution");
+
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+
+            Console.Read();
+        }
+        static void Process(object callback)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                for (int j = 0; j < 100000; j++)
+                {
+                }
+            }
+        }
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+    }
+}
